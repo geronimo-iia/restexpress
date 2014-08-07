79455262a29f2dd4139b29624f523147e4bc9e95
Jerome Guibert <jguibert@intelligents-ia.com>
2014-08-07 11:03:23 +0200

    refactor response wrapper

--- core/src/main/java/org/restexpress/response/ResponseWrapper.java
+++ core/src/main/java/org/restexpress/response/ResponseWrapper.java
@@ -1,36 +1,15 @@
 /**
- *        Licensed to the Apache Software Foundation (ASF) under one
- *        or more contributor license agreements.  See the NOTICE file
- *        distributed with this work for additional information
- *        regarding copyright ownership.  The ASF licenses this file
- *        to you under the Apache License, Version 2.0 (the
- *        "License"); you may not use this file except in compliance
- *        with the License.  You may obtain a copy of the License at
- *
- *          http://www.apache.org/licenses/LICENSE-2.0
- *
- *        Unless required by applicable law or agreed to in writing,
- *        software distributed under the License is distributed on an
- *        "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *        KIND, either express or implied.  See the License for the
- *        specific language governing permissions and limitations
- *        under the License.
- *
- */
-/*
- Copyright 2011, Strategic Gains, Inc.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership. The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License
+ * at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
  */
 package org.restexpress.response;
 
@@ -39,12 +18,18 @@ import org.restexpress.Response;
 /**
  * {@link ResponseWrapper} define methods to wrap a {@link Response}.
  * 
+ * @author <a href="mailto:jguibert@intelligents-ia.com" >Jerome Guibert</a>
  * @author toddf
  * @since Feb 10, 2011
  */
 public interface ResponseWrapper {
 
-	public Object wrap(Response response);
+    /**
+     * Wrap the response.
+     * 
+     * @param response
+     * @return wrapper response object/
+     */
+    public Object wrap(Response response);
 
-	public boolean addsBodyContent(Response response);
 }
