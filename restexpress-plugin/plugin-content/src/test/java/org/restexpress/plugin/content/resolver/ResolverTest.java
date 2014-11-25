1f188c45e2b4d3c77e26dfb963f8540ee893b799
Jerome Guibert <jerome.guibert@fullsix.com>
2014-11-25 15:44:58 +0100

    adding test on resolver class

--- restexpress-plugin/plugin-content/src/test/java/org/restexpress/plugin/content/resolver/ResolverTest.java
+++ restexpress-plugin/plugin-content/src/test/java/org/restexpress/plugin/content/resolver/ResolverTest.java
@@ -1,9 +1,43 @@
 package org.restexpress.plugin.content.resolver;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test class for {@link ResolverTest}.
+ * 
+ * @author <a href="mailto:jguibert@intelligents-ia.com" >Jerome Guibert</a>
+ * 
+ */
 public class ResolverTest {
 
     public ResolverTest() {
-        // TODO Auto-generated constructor stub
     }
 
+    @Test
+    public void checkResolverWithoutExcludedPart() {
+        final Resolver resolver = new Resolver("", null);
+        
+        assertTrue(resolver.match("/root"));
+        assertFalse(resolver.match("boot"));
+        assertEquals("/root/", resolver.resolve("/root/"));
+        assertEquals("/Root/", resolver.resolve("/Root/"));
+        assertEquals("/root/a", resolver.resolve("/root/a"));
+        assertEquals("/rOot/a", resolver.resolve("/rOot/a"));
+        assertEquals("/rOot/a/b/c/d", resolver.resolve("/rOot/a/b/c/d"));
+    }
+
+    @Test
+    public void checkResolverWithExcludedPart() {
+        final Resolver resolver = new Resolver("/root", new String[] {"/root/b"});
+        assertTrue(resolver.match("/root"));
+        assertFalse(resolver.match("boot"));
+        
+        assertTrue(resolver.match("/root/a"));
+        assertEquals("/a", resolver.resolve("/root/a"));
+        assertFalse(resolver.match("/root/b"));
+        assertFalse(resolver.match("/root/b/a"));
+        
+    }
 }
