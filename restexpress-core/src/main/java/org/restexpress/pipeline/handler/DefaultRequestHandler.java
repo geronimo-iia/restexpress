a8a5e7f37675a2e450899c0116f3b06023e3facd
Jerome Guibert <jguibert@intelligents-ia.com>
2014-12-04 11:11:53 +0100

    fix default content type

--- restexpress-core/src/main/java/org/restexpress/pipeline/handler/DefaultRequestHandler.java
+++ restexpress-core/src/main/java/org/restexpress/pipeline/handler/DefaultRequestHandler.java
@@ -47,87 +47,92 @@ import org.restexpress.util.HttpSpecification;
 @Sharable
 public class DefaultRequestHandler extends AbstractRequestHandler {
 
-    private final RouteResolver routeResolver;
-    private final ResponseProcessorManager responseProcessorManager;
+	private final RouteResolver routeResolver;
+	private final ResponseProcessorManager responseProcessorManager;
 
-    /**
-     * Build a new instance of {@link DefaultRequestHandler}.
-     * 
-     * @param routeResolver {@link RouteResolver}.
-     * @param responseProcessorManager {@link ResponseProcessorManager}
-     * @param responseWriter {@link HttpResponseWriter}
-     * @param enforceHttpSpec
-     */
-    public DefaultRequestHandler(final RouteResolver routeResolver, //
-            final ResponseProcessorManager responseProcessorManager,//
-            final HttpResponseWriter responseWriter, final boolean enforceHttpSpec) {
-        super(responseWriter, enforceHttpSpec);
-        this.routeResolver = routeResolver;
-        this.responseProcessorManager = responseProcessorManager;
-    }
+	/**
+	 * Build a new instance of {@link DefaultRequestHandler}.
+	 * 
+	 * @param routeResolver
+	 *            {@link RouteResolver}.
+	 * @param responseProcessorManager
+	 *            {@link ResponseProcessorManager}
+	 * @param responseWriter
+	 *            {@link HttpResponseWriter}
+	 * @param enforceHttpSpec
+	 */
+	public DefaultRequestHandler(final RouteResolver routeResolver, //
+			final ResponseProcessorManager responseProcessorManager,//
+			final HttpResponseWriter responseWriter, final boolean enforceHttpSpec) {
+		super(responseWriter, enforceHttpSpec);
+		this.routeResolver = routeResolver;
+		this.responseProcessorManager = responseProcessorManager;
+	}
 
-    /**
-     * Build a new instance of {@link DefaultRequestHandler}.
-     * 
-     * @param routeResolver {@link RouteResolver}.
-     * @param responseProcessorSettingResolver {@link ResponseProcessorSettingResolver}
-     * @param responseWriter {@link HttpResponseWriter}
-     * @param enforceHttpSpec
-     */
-    public DefaultRequestHandler(final RouteResolver routeResolver, //
-            final HttpResponseWriter responseWriter, final boolean enforceHttpSpec) {
-        this(routeResolver, new ResponseProcessorManager(), responseWriter, enforceHttpSpec);
-    }
+	/**
+	 * Build a new instance of {@link DefaultRequestHandler}.
+	 * 
+	 * @param routeResolver
+	 *            {@link RouteResolver}.
+	 * @param responseProcessorSettingResolver
+	 *            {@link ResponseProcessorSettingResolver}
+	 * @param responseWriter
+	 *            {@link HttpResponseWriter}
+	 * @param enforceHttpSpec
+	 */
+	public DefaultRequestHandler(final RouteResolver routeResolver, //
+			final HttpResponseWriter responseWriter, final boolean enforceHttpSpec) {
+		this(routeResolver, new ResponseProcessorManager(), responseWriter, enforceHttpSpec);
+	}
 
-    /**
-     * Testing purpose.
-     * 
-     * @return {@link SerializationProvider}.
-     */
-    public SerializationProvider serializationProvider() {
-        return responseProcessorManager;
-    }
+	/**
+	 * Testing purpose.
+	 * 
+	 * @return {@link SerializationProvider}.
+	 */
+	public SerializationProvider serializationProvider() {
+		return responseProcessorManager;
+	}
 
-    @Override
-    protected void resolveResponseProcessor(final MessageContext context) {
-        final ResponseProcessorSetting setting = responseProcessorManager.resolve(context.getRequest(), context.getResponse(), false);
-        context.setResponseProcessorSetting(setting);
-    }
+	@Override
+	protected void resolveResponseProcessor(final MessageContext context) {
+		final ResponseProcessorSetting setting = responseProcessorManager.resolve(context.getRequest(), context.getResponse(), false);
+		context.setResponseProcessorSetting(setting);
+	}
 
-    @Override
-    protected void resolveRoute(final MessageContext context) {
-        final Action action = routeResolver.resolve(context);
-        context.setAction(action);
-    }
+	@Override
+	protected void resolveRoute(final MessageContext context) {
+		final Action action = routeResolver.resolve(context);
+		context.setAction(action);
+	}
 
-    @Override
-    protected Request createRequest(final MessageEvent event, final ChannelHandlerContext context) {
-        return new Request((HttpRequest) event.getMessage(), routeResolver, responseProcessorManager,
-                (InetSocketAddress) event.getRemoteAddress());
-    }
+	@Override
+	protected Request createRequest(final MessageEvent event, final ChannelHandlerContext context) {
+		return new Request((HttpRequest) event.getMessage(), routeResolver, responseProcessorManager, (InetSocketAddress) event.getRemoteAddress());
+	}
 
-    @Override
-    protected Response createResponse() {
-        return new Response();
-    }
+	@Override
+	protected Response createResponse() {
+		return new Response();
+	}
 
-    @Override
-    protected void serializeResponse(final MessageContext context, final boolean force) {
-        final Response response = context.getResponse();
-        if (HttpSpecification.isContentTypeAllowed(response)) {
-            if (response.isSerialized()) {
-                ResponseProcessorSetting settings = context.getResponseProcessorSetting();
-                if (settings == null && force) {
-                    settings = responseProcessorManager.resolve(context.getRequest(), response, force);
-                }
-                if (settings != null) {
-                    settings.serialize(response);
-                }
-                // add default content type if none was provided only if we serialize something.
-                if (!response.hasHeader(HttpHeaders.Names.CONTENT_TYPE)) {
-                    response.setContentType(MediaType.TEXT_PLAIN.withCharset(CharacterSet.UTF_8.getCharsetName()));
-                }
-            }
-        }
-    }
+	@Override
+	protected void serializeResponse(final MessageContext context, final boolean force) {
+		final Response response = context.getResponse();
+		if (HttpSpecification.isContentTypeAllowed(response)) {
+			if (response.isSerialized()) {
+				ResponseProcessorSetting settings = context.getResponseProcessorSetting();
+				if (settings == null && force) {
+					settings = responseProcessorManager.resolve(context.getRequest(), response, force);
+				}
+				if (settings != null) {
+					settings.serialize(response);
+				}
+			}
+			// add default content type if none was provided only if content type is allowed
+			if (!response.hasHeader(HttpHeaders.Names.CONTENT_TYPE)) {
+				response.setContentType(MediaType.TEXT_PLAIN.withCharset(CharacterSet.UTF_8.getCharsetName()));
+			}
+		}
+	}
 }
